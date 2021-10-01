e08d1ffd58e021a6e9e84d969bd393792ce58e7c
Chiro Liang <chiro2001@163.com>
2021-10-01 09:54:48 +0800

    update nginx

--- backend/install.sh
+++ backend/install.sh
@@ -1,2 +1,4 @@
-sudo cp ./gbk.nginx /etc/nginx/sites-available/gbk
+sudo cp ./meiri.nginx /etc/nginx/sites-available/meiri
+cd /etc/nginx/sites-enabled/
+sudo ln -s ../sites-available/meiri meiri
 sudo systemctl restart nginx.service
